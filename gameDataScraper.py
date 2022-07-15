339cebeed46011c47a7094c2f87d8d30024ca29e
splendor-kill <splendor.kill@gmail.com>
2022-07-15 18:19:48 +0800

    multi-thread download

--- gameDataScraper.py
+++ gameDataScraper.py
@@ -6,6 +6,8 @@
 
 import argparse
 import os
+from concurrent.futures import ThreadPoolExecutor
+from time import time
 
 import requests
 
@@ -13,6 +15,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('start', type=int, default=58000000, help='where to start downloading')
 parser.add_argument('--num', type=int, default=10000, help='how many to download')
 parser.add_argument('--data_dir', type=str, help='where to save')
+parser.add_argument('--n_concur', type=int, default=1, help='how many processes/threads')
 
 args = parser.parse_args()
 script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -20,19 +23,26 @@ data_dir = args.data_dir if args.data_dir else os.path.join(script_dir, 'data')
 os.makedirs(data_dir, exist_ok=True)
 
 start = args.start
-end = start + args.num
+n_replays = args.num
+end = start + n_replays
 
-for gameID in range(start, end):
-    # targetURL = f'http://www.playok.com/zh/game.phtml/{gameID}.txt?g=xq'
-    targetURL = f'https://www.playok.com/p/?g=xq{gameID}.txt'
+
+def download(game_id):
+    # targetURL = f'http://www.playok.com/zh/game.phtml/{game_id}.txt?g=xq'
+    targetURL = f'https://www.playok.com/p/?g=xq{game_id}.txt'
     try:
         res = requests.get(targetURL)
         res.raise_for_status()
     except Exception:
-        continue
-    file_path = os.path.join(data_dir, f'{gameID}.txt')
+        return
+    file_path = os.path.join(data_dir, f'{game_id}.txt')
     with open(file_path, 'wb') as fp:
         for chunk in res.iter_content(100000):
             fp.write(chunk)
+    print(f'got game replay {game_id}')
 
-    print(f'got game replay {gameID}')
+
+start_time = time()
+with ThreadPoolExecutor(max_workers=args.n_concur) as executor:
+    executor.map(download, range(start, end))
+print(f'download {n_replays} spend {time() - start_time} sec.')
